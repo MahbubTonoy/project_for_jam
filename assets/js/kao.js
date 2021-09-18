9134cf431e9d6cf2e7b177faa108df9fcc530c9e
MahbubTonoy <tnymrbadboy@gmail.com>
2021-09-18 22:33:00 +0600

    eyeblinking bug fixed

--- assets/js/kao.js
+++ assets/js/kao.js
@@ -1,25 +1,57 @@
-//elements move (parallax)
+let min = 3;
+let max = 5; //eye blink min and max in second
+let blinkInterval = 100;
+
+
+var finalMin = min; // value will change here
+var finalMax = max; // value will change here
+var finalBlinkInterval = blinkInterval;
+
+//eye blink interval
+let interval = setInterval(eyeBlink, getRandomArbitrary(finalMin, finalMax));
+
 document.querySelector("body").addEventListener("mousemove", (e) => {
   //minimum 20 - less number, more sensitivity;
   let sensitivity = 100;
   let okutiSensitivity = 400;
   
-
-
-
+  let windowWidthHalf = window.innerWidth / 2;
+  let windowHeightHalf = window.innerHeight / 2;
+  
   elementMove(".responsive-position-left", e, sensitivity); //left eye
   elementMove(".responsive-position-right", e, sensitivity); //right eye
   elementMove(".okuti", e, okutiSensitivity); //mouth
-
+  
   elementShrink(".omeme-lash-left", e, "left"); //left eye lash
   elementShrink(".omeme-lash-right", e, "right"); //right eye lash
-
+  
   elementShrink(".okuti-shape-left", e, "left"); //mouth left part
   elementShrink(".okuti-shape-right", e, "right"); //mouth right part
-
+  
   mouth(e); //change mouth position
+  
+  // //increase eye blinking when mouse is center
+  // if (
+  //   e.pageX < windowWidthHalf + 100 &&
+  //   e.pageX > windowWidthHalf - 100 &&
+  //   e.pageY < windowHeightHalf
+  //   ) {
+  //     finalMin = 1;
+  //   finalMax = 1;
+  //   clearInterval(interval);
+  //   interval = setInterval(eyeBlink, getRandomArbitrary(finalMin, finalMax));
+  // } else {
+  //   finalMin = min;
+  //   finalMax = max;
+  //   clearInterval(interval);
+  //   interval = setInterval(eyeBlink, getRandomArbitrary(finalMin, finalMax));
+  // }
 });
 
+
+
+
+//elements move (parallax)
 function elementMove(elementClass, e, sensitivity) {
   let element = document.querySelector(elementClass);
   if (element == undefined) {
@@ -47,14 +79,11 @@ function elementShrink(elementClass, e, side) {
 
   let windowWidthHalf = window.innerWidth / 2;
 
-  let elementPosX =
-    element.getBoundingClientRect().left + element.offsetWidth / 2;
   let cursorDirectionX = (e.pageX - windowWidthHalf) / 30;
 
   if (side == "left") {
     if (e.pageX < windowWidthHalf) {
-      element.style.width = Math.abs(cursorDirectionX) + "px solid #fff";
-
+      element.style.borderLeft = Math.abs(cursorDirectionX) + "px solid #fff";
     }
   }
   if (side == "right") {
@@ -69,8 +98,6 @@ function mouth(e) {
   let mouthRight = document.querySelector(".okuti-right");
 
   let windowWidthHalf = window.innerWidth / 2;
-
-  // let elementPosX = mouthCenter.getBoundingClientRect().left + mouthCenter.offsetWidth / 2;
 
   if (windowWidthHalf > e.pageX) {
     let changeLimit = (windowWidthHalf - e.pageX) / 500;
@@ -92,20 +119,21 @@ function mouth(e) {
 }
 
 
-let min = 3, max = 5; //define min anx max time of eye blink in second
 
-setInterval(()=>{
+//eye blinking functionality
+function eyeBlink() {
   let leftEye = document.querySelector(".omeme-left");
   let rightEye = document.querySelector(".omeme-right");
-
+  
   leftEye.style.borderTop = leftEye.offsetHeight + "px solid #fff";
   rightEye.style.borderTop = rightEye.offsetHeight + "px solid #fff";
-  setInterval(()=>{
+  setTimeout(() => {
     leftEye.style.borderTop = "0px solid #fff";
     rightEye.style.borderTop = "0px solid #fff";
-  }, 300);
+  }, finalBlinkInterval);
+}
 
-}, getRandomArbitrary(min, max));
+
 
 
 
@@ -113,5 +141,6 @@ setInterval(()=>{
 function getRandomArbitrary(min, max) {
   min *= 1000;
   max *= 1000;
+  // console.log(min, max);
   return Math.round(Math.random() * (max - min) + min);
 }
